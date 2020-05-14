177bb53097cb436cdb837f052799b7f6c562ad7b
jakub <jakubstankowski@protonmail.com>
2020-05-14 17:30:41 +0200

    init commit

---
+++ ExpensesTracker/Print/ConsolePrinter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpensesTracker.Print
+{
+    class ConsolePrinter
+    {
+        public static void Menu()
+        {
+            Console.WriteLine("Welcome to expenses tracker!");
+            Console.WriteLine("1. Set your money");
+            Console.WriteLine("2. Exit program");
+          
+        }
+    }
+}
---
+++ ExpensesTracker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ExpensesTracker.Print;
+
+namespace ExpensesTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            ConsolePrinter.Menu();
+            var option = Console.ReadKey().Key;
+
+            while (true)
+            {
+                switch (option)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Add money");
+                        break;
+                    case ConsoleKey.D2:
+                        return;
+                        break;
+
+                }
+             }
+           
+            /*  while (true)
+              {
+                  string test = Console.ReadLine();
+
+                  if (test == "test")
+                  {
+                      Console.WriteLine("work!");
+                  } else
+                  {
+                      return;
+                  }
+
+              }*/
+        }
+    }
+}
